97a8ba34c35c9d72f629e2a94a5aa5e41c8dc4d1
Dhrupad Chakraborty <dhrupadchakraborty123@gmail.com>
2024-09-14 20:49:50 +0530

    Project before applying Liskov's Substitution Principle

---
+++ 3_LiskovSubstitutionPrinciple/src/main/java/com/dhruv/Main.java
@@ -0,0 +1,32 @@
+package com.dhruv;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Rectangle rectangle=new Rectangle(10,20);
+        System.out.println(rectangle.computeArea());
+
+        Square square=new Square(10);
+
+        System.out.println(square.computeArea());
+
+        useRectangle(rectangle);
+
+        useRectangle(square);
+
+
+    }
+
+    private static void useRectangle(Rectangle rectangle) {
+
+        rectangle.setWidth(20);
+        rectangle.setHeight(30);
+
+        if(rectangle.getHeight()!=30)
+            System.out.println("Height Not equal to 30");
+
+        if(rectangle.getWidth()!=20)
+            System.out.println("Width Not equal to 20");
+
+    }
+}
---
+++ 3_LiskovSubstitutionPrinciple/src/main/java/com/dhruv/Square.java
@@ -0,0 +1,25 @@
+package com.dhruv;
+
+public class Square extends  Rectangle{
+
+    public Square(int side) {
+        super(side, side);
+    }
+
+    @Override
+    public void setWidth(int width){
+        setSide(width);
+    }
+
+    @Override
+    public void setHeight(int height){
+        setSide(height);
+    }
+
+    private void setSide(int side) {
+        super.setWidth(side);
+        super.setHeight(side);
+    }
+
+
+}
