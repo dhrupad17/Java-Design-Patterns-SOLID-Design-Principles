6c572794eb0646916e7915e84b733ab831547a04
Dhrupad Chakraborty <dhrupadchakraborty123@gmail.com>
2024-09-14 20:24:09 +0530

    Project without applying Open/Closed Principle

---
+++ 2_Open-ClosedPrinciple/src/main/java/com/dhruv/entity/InternetSessionHistory.java
@@ -0,0 +1,63 @@
+package com.dhruv.entity;
+
+import java.time.LocalDateTime;
+import java.util.*;
+
+public class InternetSessionHistory {
+
+    public static class InternetSession {
+
+        private LocalDateTime begin;
+
+        private Long subscriberId;
+
+        private Long dataUsed;
+
+        public InternetSession(Long subscriberId, LocalDateTime begin, long dataUsed) {
+            this.begin = begin;
+            this.dataUsed = dataUsed;
+            this.subscriberId = subscriberId;
+        }
+
+        /**
+         * @return the begin
+         */
+        public LocalDateTime getBegin() {
+            return begin;
+        }
+
+        /**
+         * @return the data used
+         */
+        public long getDataUsed() {
+            return dataUsed;
+        }
+
+        /**
+         * @return the subscriber
+         */
+        public Long getSubscriberId() {
+            return subscriberId;
+        }
+
+    }
+    private static final Map<Long, List<InternetSession>> SESSIONS = new HashMap<>();
+
+    public synchronized static List<InternetSession> getCurrentSessions(Long subscriberId) {
+        if(!SESSIONS.containsKey(subscriberId)) {
+            return Collections.emptyList();
+        }
+        return SESSIONS.get(subscriberId);
+    }
+
+    public synchronized static void addSession(Long subscriberId, LocalDateTime begin, long dataUsed) {
+        List<InternetSession> sessions;
+        if(!SESSIONS.containsKey(subscriberId)) {
+            sessions = new LinkedList<>();
+            SESSIONS.put(subscriberId, sessions);
+        } else {
+            sessions = SESSIONS.get(subscriberId);
+        }
+        sessions.add(new InternetSession(subscriberId, begin, dataUsed));
+    }
+}
---
+++ 2_Open-ClosedPrinciple/src/main/java/com/dhruv/model/PhoneSubscriber.java
@@ -0,0 +1,54 @@
+package com.dhruv.model;
+
+import com.dhruv.entity.CallHistory;
+
+import java.util.List;
+
+public class PhoneSubscriber {
+
+    private Long subscriberId;
+
+    private String address;
+
+    private Long phoneNumber;
+
+    private int baseRate;
+
+    public Long getSubscriberId() {
+        return subscriberId;
+    }
+
+    public void setSubscriberId(Long subscriberId) {
+        this.subscriberId = subscriberId;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Long getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(Long phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public int getBaseRate() {
+        return baseRate;
+    }
+
+    public void setBaseRate(int baseRate) {
+        this.baseRate = baseRate;
+    }
+
+    public double calculateBill(){
+        List<CallHistory.Call> sessions=CallHistory.getCurrentCalls(subscriberId);
+        long totalDuration=sessions.stream().mapToLong(CallHistory.Call:: getDuration).sum();
+        return totalDuration*baseRate/100;
+    }
+}
