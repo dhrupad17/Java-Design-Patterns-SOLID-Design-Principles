062a3b0ae432cbcb39ff3f570c5d11823794e16c
Dhrupad Chakraborty <dhrupadchakraborty123@gmail.com>
2024-09-22 12:50:19 +0530

    Project after applying Simple Factory Pattern

--- 8_SimpleFactoryPattern/src/main/java/com/dhruv/Client.java
+++ 8_SimpleFactoryPattern/src/main/java/com/dhruv/Client.java
@@ -1,8 +1,12 @@
 package com.dhruv;
 
+import com.dhruv.factory.PostFactory;
+import com.dhruv.model.Post;
+
 public class Client {
 
     public static void main(String[] args) {
-
+        Post post= PostFactory.createPost("news");
+        System.out.println(post);
     }
 }
--- 8_SimpleFactoryPattern/src/main/java/com/dhruv/factory/PostFactory.java
+++ 8_SimpleFactoryPattern/src/main/java/com/dhruv/factory/PostFactory.java
@@ -1,4 +1,22 @@
 package com.dhruv.factory;
 
+import com.dhruv.model.BlogPost;
+import com.dhruv.model.NewsPost;
+import com.dhruv.model.Post;
+import com.dhruv.model.ProductPost;
+
 public class PostFactory {
+
+    public static Post createPost(String type){
+        switch (type){
+            case "blog":
+                return new BlogPost();
+            case "news":
+                return new NewsPost();
+            case "product":
+                return new ProductPost();
+            default:
+                throw new IllegalArgumentException("Post Type is unknown");
+        }
+    }
 }
