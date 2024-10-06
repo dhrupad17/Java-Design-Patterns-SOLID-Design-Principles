ae1228fd27ab6ea2df405754277c46ec9171bcb2
Dhrupad Chakraborty <dhrupadchakraborty123@gmail.com>
2024-10-06 19:07:16 +0530

    Project before applying Prototype Design Pattern

---
+++ 10_PrototypeDesignPattern/src/main/java/com/dhruv/GameUnit.java
@@ -0,0 +1,25 @@
+package com.dhruv;
+
+public abstract class GameUnit {
+
+    private Point3D position;
+
+    public GameUnit(){
+        position=Point3D.ZERO;
+    }
+
+    public GameUnit(float x,float y,float z){
+        position=new Point3D(x,y,z);
+    }
+
+    public void move(Point3D direction, float distance) {
+        Point3D finalMove = direction.normalize();
+        finalMove = finalMove.multiply(distance);
+        position = position.add(finalMove);
+    }
+
+    public Point3D getPosition(){
+        return position;
+    }
+
+}
---
+++ 10_PrototypeDesignPattern/src/main/java/com/dhruv/Swordsman.java
@@ -0,0 +1,15 @@
+package com.dhruv;
+
+public class Swordsman extends GameUnit{
+
+    private String state="idle";
+
+    public void attack(){
+        this.state="attacking";
+    }
+
+    @Override
+    public String toString() {
+        return "Swordsman "+state+" @ "+getPosition();
+    }
+}
